df68ae2fce5987df7959b5bcb6364e553b516f6a
true <gbg292526@gmail.com>
2026-07-07 23:17:32 -0300

    refactor para prod em build.sh

--- build.sh
+++ build.sh
@@ -4,3 +4,14 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = __import__('os').environ.get('DJANGO_SUPERUSER_USERNAME', '')
+password = __import__('os').environ.get('DJANGO_SUPERUSER_PASSWORD', '')
+email = __import__('os').environ.get('DJANGO_SUPERUSER_EMAIL', '')
+if username and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print('Superuser criado')
+"
