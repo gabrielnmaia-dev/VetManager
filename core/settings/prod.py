812627fdd886057bca8bafcd941f22c56f356105
gabDev <107522512+gabrielnmaia-dev@users.noreply.github.com>
2026-07-08 08:44:19 -0300

    Atualizar o prod.py

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -37,6 +37,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGIN_REGEXES = [
-    r"^https://vetmaneger-front.*\.vercel\.app$",
+CORS_ALLOWED_ORIGINS = [
+    "https://vetmanager-letvet.netlify.app",
 ]
